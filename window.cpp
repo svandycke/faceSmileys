f808df29dda8d70ff0c457e207a21c22709df251
svandycke <svandycke@gmail.com>
2016-11-29 13:27:24 +0100

    Placage d'image

--- window.cpp
+++ window.cpp
@@ -16,7 +16,6 @@ using namespace cv;
 using namespace std;
 
 // Variables globales
-Mat smiley;
 
 int main(int argc, char ** argv) {
   CascadeClassifier * face_cc = NULL;
@@ -34,14 +33,27 @@ int main(int argc, char ** argv) {
   cap->set(CV_CAP_PROP_FRAME_WIDTH,  500);
   cap->set(CV_CAP_PROP_FRAME_HEIGHT, 500);
   Mat ci, gsi;
-  namedWindow("Face detection", CV_WINDOW_AUTOSIZE);
+
+  cv::Mat image = cv::imread("mignonSmile.png",cv::IMREAD_UNCHANGED);
+
+
+  namedWindow("Face detection", WINDOW_NORMAL);
   while(1) {
     *cap >> ci;
     vector<Rect> faces;
     cvtColor(ci, gsi, COLOR_BGR2GRAY);
     face_cc->detectMultiScale(gsi, faces, 1.3, 5);
     for (vector<Rect>::iterator fc = faces.begin(); fc != faces.end(); ++fc) {
-      rectangle(ci, (*fc).tl(), (*fc).br(), Scalar(0, 255, 0), 2, CV_AA);
+      
+       if(image.data )         
+      {
+        cv::imshow( "Face detection", image ); 
+        cv::waitKey(0);
+      }else{
+        rectangle(ci, (*fc).tl(), (*fc).br(), Scalar(0, 255, 0), 2, CV_AA);
+      }
+
+
     }
     imshow("Face detection", ci);
     if((waitKey(10) & 0xFF) == 27)
